6d4380148c0d26e07b7cb9ef01f0242ac8423fe8
surcovilie <101186767+surcovilie@users.noreply.github.com>
2022-04-12 21:30:28 +0300

    lab4erroneous
    lab4erroneous

--- lab3lab4/src/store/RootStore.store.ts
+++
@@ -1,9 +0,0 @@
-import { types } from 'mobx-state-tree'
-import { ContentModel } from '../models/Content.model'
-
-
-const RootStore = types.model('RootStore', {
-    contents: types.array(ContentModel)
-})
-
-export default RootStore
